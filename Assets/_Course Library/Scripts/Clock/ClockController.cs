af2c02e0ec8ca331ed4d8027881344d384650ae0
thanh <hoanthanh@spiritbomb.co>
2025-02-10 17:53:12 +0700

    feat: clock controller

--- Assets/_Course Library/Scripts/Clock/ClockController.cs
+++ Assets/_Course Library/Scripts/Clock/ClockController.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ClockController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform hourHand;
+    public Transform minuteHand;
+    public Transform secondHand;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        DateTime currentTime = DateTime.Now;
+
+        float hourAngle = (currentTime.Hour % 12 + currentTime.Minute / 60f) * 30f;
+        hourHand.localRotation = Quaternion.Euler(0, hourAngle, 0); 
+
+        float minuteAngle = currentTime.Minute * 6f;
+        minuteHand.localRotation = Quaternion.Euler(0, minuteAngle, 0); 
+
+        float secondAngle = currentTime.Second * 6f;
+        secondHand.localRotation = Quaternion.Euler(0, secondAngle, 0); 
     }
 }
